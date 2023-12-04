edd5b1f56a9b1c410c9b5e1c2251322da5c85ada
Somnath Bera <somnathbera1919@gmail.com>
2023-12-04 12:07:39 +0530

    simple 'Hello World' app

---
+++ FirstProject/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+{
+  /* <Section title="">
+<Button onPress={() => setCount(count + 1)} title="Increase" />
+</Section>
+<Section title="">
+<Text>You clicked {count} times</Text>
+</Section>
+<Section title="">
+<Button onPress={() => setCount(count - 1)} title="Decrease" />
+</Section> */
+}
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
